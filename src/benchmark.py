9068abb2d1c2a932fcfba9dd587ab64b868991d4
JordanRichard <55063156+JordanRichard@users.noreply.github.com>
2022-03-31 22:51:50 -0300

    Switched from ThreadPoolExecutor to ProcessPoolExecutor to address low utilizaion, seems to be performing as intended now.

--- src/benchmark.py
+++ src/benchmark.py
@@ -12,7 +12,7 @@ import wmi
 import psutil
 from time import perf_counter
 from concurrent.futures import ProcessPoolExecutor
-from concurrent.futures import ThreadPoolExecutor
+
 
 #from pyJoules.energy_meter import measure_energy
 
@@ -34,95 +34,95 @@ def lehmer(p: int) -> bool:
 
 
 
-
-#Initial printout of system information and menu screen schowing benchamrking options
-print("_________________________________________________________________________________")
-print("------------------------------System Information---------------------------------")
-print(f"\tOS: {sys.system} {sys.release} ") #
-print(f"\tMachine Name: {sys.node}")
-print(f"\tVersion: {sys.version}")
-print(f"\tCPU: {sys.processor}")
-print("\tNumber of Cores: " + str(psutil.cpu_count()))
-print(f"\tRAM: {psutil.virtual_memory()}")
-print("---------------------------------------------------------------------------------")
-
-
-modeSelect = 0;
-print("Welcome to ParaBench! Please select what benchmarking mode you would like to use." + '\n')
-modeSelect = int(input("[1] -> " + mode1 + '\n' + "[2] -> " + mode2
-    + "\n[9] -> Exit\n_________________________________________________________________________________\n"))
+if __name__ == '__main__':
+    #Initial printout of system information and menu screen schowing benchamrking options
+    print("_________________________________________________________________________________")
+    print("------------------------------System Information---------------------------------")
+    print(f"\tOS: {sys.system} {sys.release} ") #
+    print(f"\tMachine Name: {sys.node}")
+    print(f"\tVersion: {sys.version}")
+    print(f"\tCPU: {sys.processor}")
+    print("\tNumber of Cores: " + str(psutil.cpu_count()))
+    print(f"\tRAM: {psutil.virtual_memory()}")
+    print("---------------------------------------------------------------------------------")
 
 
-#User selects Integer benchmarking mode
-if modeSelect == 1:
-    print("User Selected " + mode1)
-
-    #Printout of selection for order of magnitude
-    print("[1] -> First 1x10^2 Primes\n" +  "[2] -> First 1x10^3 Primes\n"
-        +"[3] -> First 1x10^4 Primes\n" + "[4] -> First 1x10^5 Primes\n" + "[5] -> First 1x10^6 Primes\n")
-    mersenneOrder = int(input("Please Select an option\n"))
+    modeSelect = 0;
+    print("Welcome to ParaBench! Please select what benchmarking mode you would like to use." + '\n')
+    modeSelect = int(input("[1] -> " + mode1 + '\n' + "[2] -> " + mode2
+        + "\n[9] -> Exit\n_________________________________________________________________________________\n"))
 
 
-    if mersenneOrder == 1:
-        print("Starting Benchmark...")
-        with ThreadPoolExecutor(15) as executor:
-            timeStart = perf_counter()
+    #User selects Integer benchmarking mode
+    if modeSelect == 1:
+        print("User Selected " + mode1)
 
-            for result in executor.map(lehmer,range(2,100)):
-                print(result)
-            timeStop = perf_counter()
-
-            print("1E2 Benchmark Complete in ",timeStop-timeStart)
+        #Printout of selection for order of magnitude
+        print("[1] -> First 1x10^2 Primes\n" +  "[2] -> First 1x10^3 Primes\n"
+            +"[3] -> First 1x10^4 Primes\n" + "[4] -> First 1x10^5 Primes\n" + "[5] -> First 1x10^6 Primes\n")
+        mersenneOrder = int(input("Please Select an option\n"))
 
 
-    if mersenneOrder == 2:
-        print("Starting Benchmark...")
-        with ThreadPoolExecutor(15) as executor:
-            timeStart = perf_counter()
+        if mersenneOrder == 1:
+            print("Starting Benchmark...")
+            with ProcessPoolExecutor(15) as executor:
+                timeStart = perf_counter()
 
-            for result in executor.map(lehmer,range(2,1000)):
-                print(result)
-            timeStop = perf_counter()
-            print("1E3 Benchmark Complete!!", timeStop-timeStart)
+                for result in executor.map(lehmer,range(2,100)):
+                    print(result)
+                timeStop = perf_counter()
 
-
-    if mersenneOrder == 3:
-        print("Starting Benchmark...")
-        with ThreadPoolExecutor() as executor:
-            timeStart = perf_counter()
-
-            for result in executor.map(lehmer,range(2,10000)):
-                print(result)
-            timeStop = perf_counter()
-            print("1E4 Benchmark Complete!!", timeStop-timeStart)
+                print("1E2 Benchmark Complete in ",timeStop-timeStart)
 
 
-    if mersenneOrder == 4:
-        print("Starting Benchmark...")
-        with ThreadPoolExecutor(15) as executor:
-            timeStart = perf_counter()
+        if mersenneOrder == 2:
+            print("Starting Benchmark...")
+            with ProcessPoolExecutor(15) as executor:
+                timeStart = perf_counter()
 
-            for result in executor.map(lehmer,range(2,100000)):
-                print(result)
-            timeStop = perf_counter()
-            print("1E5 Benchmark Complete!!", timeStop-timeStart)
-
-
-    if mersenneOrder == 5:
-        print("Starting Benchmark...")
-        with ThreadPoolExecutor(15) as executor:
-            timeStart = perf_counter()
-
-            for result in executor.map(lehmer,range(2,1000000)):
-                print(result)
-            timeStop = perf_counter()
-            print("1E6 Benchmark Complete!!", timeStop-timeStart)
-
-        #Single-threaded test (DEPRECATED)
-        #for x in range(2,1000000):
-        #    if lehmer(x):
-        #        print(x)
+                for result in executor.map(lehmer,range(2,1000)):
+                    print(result)
+                timeStop = perf_counter()
+                print("1E3 Benchmark Complete!!", timeStop-timeStart)
 
 
-if modeSelect == 2:                                                         # TODO: User selects floating point mode
-    print("User Selected " + mode2)
+        if mersenneOrder == 3:
+            print("Starting Benchmark...")
+            with ProcessPoolExecutor() as executor:
+                timeStart = perf_counter()
+
+                for result in executor.map(lehmer,range(2,10000)):
+                    print(result)
+                timeStop = perf_counter()
+                print("1E4 Benchmark Complete!!", timeStop-timeStart)
+
+
+        if mersenneOrder == 4:
+            print("Starting Benchmark...")
+            with ProcessPoolExecutor(15) as executor:
+                timeStart = perf_counter()
+
+                for result in executor.map(lehmer,range(2,100000)):
+                    print(result)
+                timeStop = perf_counter()
+                print("1E5 Benchmark Complete!!", timeStop-timeStart)
+
+
+        if mersenneOrder == 5:
+            print("Starting Benchmark...")
+            with ProcessPoolExecutor(15) as executor:
+                timeStart = perf_counter()
+
+                for result in executor.map(lehmer,range(2,1000000)):
+                    print(result)
+                timeStop = perf_counter()
+                print("1E6 Benchmark Complete!!", timeStop-timeStart)
+
+            #Single-threaded test (DEPRECATED)
+            #for x in range(2,1000000):
+            #    if lehmer(x):
+            #        print(x)
+
+
+    if modeSelect == 2:                                                         # TODO: User selects floating point mode
+        print("User Selected " + mode2)
