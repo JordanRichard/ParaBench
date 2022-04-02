dde0eed8b828448858c22aad8ac650cf31f63fa7
JordanRichard <55063156+JordanRichard@users.noreply.github.com>
2022-04-01 23:37:15 -0300

    Final Build
    Added logging function and bundled with intel powergadget to facilitate testing. Cleaned up code and removed redundancies, added a progress tracker in the console window. Settled on a standard benchmark - see option [5].

--- src/benchmark.py
+++ src/benchmark.py
@@ -12,16 +12,12 @@ import wmi
 import psutil
 from time import perf_counter
 from concurrent.futures import ProcessPoolExecutor
+from datetime import datetime
 
-
-#from pyJoules.energy_meter import measure_energy
 
 sys = platform.uname()
 c = wmi.WMI()
 winSys = c.Win32_ComputerSystem()[0]
-
-mode1 = "Integer Mode"
-mode2 = "Floating-Point Mode"
 
 def lehmer(p: int) -> bool:
 
@@ -29,10 +25,29 @@ def lehmer(p: int) -> bool:
     M = (1 << p) - 1
     for i in range(p - 2):
         s = ((s * s) - 2) % M
-    return s == 0
+    #return s == 0
+        if s == 0:
+            return p
 
+#Log results and save to file
+def logging():
+    textfile = open("log "+ str(datetime.today().strftime("%d-%b-%y,%H.%M.%S"))
+        + str(datetime.today().strftime("%H.%M.%S")) +".txt","w")
+    textfile.write("------------------------------System Information---------------------------------\n")
+    textfile.write(f"\tOS: {sys.system} {sys.release} \n") #
+    textfile.write(f"\tMachine Name: {sys.node}\n")
+    textfile.write(f"\tVersion: {sys.version}\n")
+    textfile.write(f"\tCPU: {sys.processor}\n")
+    textfile.write("\tNumber of Cores: " + str(psutil.cpu_count()) + "\n")
+    textfile.write(f"\tRAM: {psutil.virtual_memory()}\n")
+    textfile.write("---------------------------------------------------------------------------------\n")
+    textfile.write("OUTPUT LOG:\n")
 
+    for i in foundNumbers:
+        textfile.write(str(i) + "\n")
+    textfile.write("Elapsed Time:   " + str(timeStop-timeStart))
 
+    textfile.close()
 
 if __name__ == '__main__':
     #Initial printout of system information and menu screen schowing benchamrking options
@@ -47,32 +62,41 @@ if __name__ == '__main__':
     print("---------------------------------------------------------------------------------")
 
 
-    modeSelect = 0;
-    print("Welcome to ParaBench! Please select what benchmarking mode you would like to use." + '\n')
-    modeSelect = int(input("[1] -> " + mode1 + '\n' + "[2] -> " + mode2
-        + "\n[9] -> Exit\n_________________________________________________________________________________\n"))
+    modeSelect = None;
+    print("Welcome to ParaBench! Please enter [1] to begin benchmarking, or [0] to exit." + '\n')
+    modeSelect = int(input("[1] -> Begin Benchmarking \n[0] -> Exit\n" +
+        "_________________________________________________________________________________\n"))
 
 
     #User selects Integer benchmarking mode
     if modeSelect == 1:
-        print("User Selected " + mode1)
+        print("Below are the available benchmarks for you to run. Searching for " +
+            "mersenne numbers Mp that generate a prime number.")
 
         #Printout of selection for order of magnitude
-        print("[1] -> First 1x10^2 Primes\n" +  "[2] -> First 1x10^3 Primes\n"
-            +"[3] -> First 1x10^4 Primes\n" + "[4] -> First 1x10^5 Primes\n" + "[5] -> First 1x10^6 Primes\n")
-        mersenneOrder = int(input("Please Select an option\n"))
-
-
+        print("[1] -> First 1 x 10^2 Primes\n[2] -> First 1 x 10^3 Primes\n[3] ->" +
+            " First 1 x 10^4 Primes \n[4] -> First 1 x 10^5 Primes\n[5] -> First 5 x 10^3 (BALANCED/STANDARD BENCHMARK)\n[0] -> Abort")
+        mersenneOrder = int(input("Please Select an option\n" +
+            "_________________________________________________________________________________\n"))
+        foundNumbers = []
+        percent = 0
         if mersenneOrder == 1:
             print("Starting Benchmark...")
+            print("Mersenne numbers Mp for that form primes:")
+
             with ProcessPoolExecutor(15) as executor:
                 timeStart = perf_counter()
 
                 for result in executor.map(lehmer,range(2,100)):
-                    print(result)
+                    if result != None:
+                        percent = (result / 100) * 100
+                        print(str(result) + "        " + str(percent) + "% Done...\n")
+                        foundNumbers.append(result)
                 timeStop = perf_counter()
 
+                #Display output to console once done
                 print("1E2 Benchmark Complete in ",timeStop-timeStart)
+                logging()
 
 
         if mersenneOrder == 2:
@@ -81,10 +105,14 @@ if __name__ == '__main__':
                 timeStart = perf_counter()
 
                 for result in executor.map(lehmer,range(2,1000)):
-                    print(result)
+                    if result != None:
+                        percent = (result / 1000) * 100
+                        print(str(result) + "        " + str(percent) + "% Done...\n")                                         # TODO: Print % complete alongside results
+                        foundNumbers.append(result)
                 timeStop = perf_counter()
-                print("1E3 Benchmark Complete!!", timeStop-timeStart)
 
+                print("1E3 Benchmark Complete!!", timeStop-timeStart)
+                logging()
 
         if mersenneOrder == 3:
             print("Starting Benchmark...")
@@ -92,9 +120,14 @@ if __name__ == '__main__':
                 timeStart = perf_counter()
 
                 for result in executor.map(lehmer,range(2,10000)):
-                    print(result)
+                    if result != None:
+                        percent = (result / 10000) * 100
+                        print(str(result) + "        " + str(percent) + "% Done...\n")
+                        foundNumbers.append(result)
                 timeStop = perf_counter()
+
                 print("1E4 Benchmark Complete!!", timeStop-timeStart)
+                logging()
 
 
         if mersenneOrder == 4:
@@ -103,26 +136,35 @@ if __name__ == '__main__':
                 timeStart = perf_counter()
 
                 for result in executor.map(lehmer,range(2,100000)):
-                    print(result)
+                    if result != None:
+                        percent = (result / 100000) * 100
+                        print(str(result) + "        " + str(percent) + "% Done...\n")
+                        foundNumbers.append(result)
                 timeStop = perf_counter()
+
                 print("1E5 Benchmark Complete!!", timeStop-timeStart)
+                logging()
 
-
+        #STANDARD BENCHMARK
         if mersenneOrder == 5:
             print("Starting Benchmark...")
             with ProcessPoolExecutor(15) as executor:
                 timeStart = perf_counter()
 
-                for result in executor.map(lehmer,range(2,1000000)):
-                    print(result)
+                for result in executor.map(lehmer,range(2,5000)):
+                    if result != None:
+                        percent = (result / 5000) * 100
+                        print(str(result) + "        " + str(percent) + "% Done...\n")
+                        foundNumbers.append(result)
                 timeStop = perf_counter()
-                print("1E6 Benchmark Complete!!", timeStop-timeStart)
 
-            #Single-threaded test (DEPRECATED)
-            #for x in range(2,1000000):
-            #    if lehmer(x):
-            #        print(x)
+                print("Benchmark Complete!!", timeStop-timeStart)
+                logging()
 
+        #Aborts if user decides to quit before benchmarking
+        if mersenneOrder == 0:
+            print("User Aborted.")
 
-    if modeSelect == 2:                                                         # TODO: User selects floating point mode
-        print("User Selected " + mode2)
+    #Ends program on user choice
+    if modeSelect == 2:
+        print("User chose to exit." )
